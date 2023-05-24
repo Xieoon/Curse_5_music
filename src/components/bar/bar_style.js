db78cf3c331b79aaedb62f79e55aead13d3566ce
Xion <malahoval99@mail.ru>
2023-05-24 14:58:16 +0300

    MUSIC

--- src/components/bar/bar_style.js
+++ src/components/bar/bar_style.js
@@ -1,15 +1,8 @@
 import styled from "styled-components";
 
 export const bar = styled.div`
-width:1883.5px;
-  display: flex;
-  flex-direction: row;
-  padding-left:36px;
-  padding-top:31px;
-  padding-bottom:28px;
-  display:flex;
-  align-items: center;
-  background-color:#181818;
+height:73px;
+
 `;
 
 export const trackSwitcher = styled.img`
@@ -21,4 +14,21 @@ export const trackPlay = styled.img`
 width: 22px;
 height: 20px;
 `
+export const progresBar = styled.input`
+width: 1920px;
+height: 5px;
+background-color:#2E2E2E;
+`
+
+export const сontrolInterface= styled.div`
+width:1883.5px;
+  display: flex;
+  flex-direction: row;
+  padding-left:36px;
+  padding-top:31px;
+  padding-bottom:28px;
+  display:flex;
+  align-items: center;
+  background-color:#181818;
+`;
  
