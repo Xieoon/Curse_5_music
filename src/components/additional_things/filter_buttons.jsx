92b9b1e4d4f68678ec67fb25b7f146e931b56178
Xion <malahoval99@mail.ru>
2023-07-12 15:03:59 +0300

    filters

--- src/components/additional_things/filter_buttons.jsx
+++ src/components/additional_things/filter_buttons.jsx
@@ -1,26 +1,89 @@
-import * as S from './filter_button_style'
-import {useState,useEffect} from 'react'
-import React from 'react'
-import { useSelector } from "react-redux";
+import * as S from "./filter_button_style";
+import { useState, useEffect } from "react";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setNameFilters,
+  setGenreFilters,
+  setDateFilters,
+  removeNameFilter,
+  removeDateFilter,
+  removeGenreFilter,
+} from "../../redux/reducers/filter_slice";
 
-function FilterButton(props){
-const theme = useSelector((state) => state.themes.value);
-const [filters,setFilters] = useState('')
-    useEffect(()=>{
-        fetch(`https://painassasin.online/catalog/track/all/`)
-          .then((response) => response.json())
-          .then((posts) => {
-            let cleanFilters = posts.map(el =>el[props.id]).filter((item, i, ar) => ar.indexOf(item) === i && item !== '-' && item);
-           setFilters(cleanFilters.map(el =><S.filterText>{el}</S.filterText>));
-          });
-      },[])
-   
-    return(
-        <S.filter>
-        <S.filterButton theme={theme} active={props.visible} id={props.id} onClick={props.clicker}>{props.text}</S.filterButton>
-        {props.visible && <S.filterDropDown><S.filterContent>{filters}</S.filterContent></S.filterDropDown>}
-        </S.filter>
-    )
+function FilterButton(props) {
+  const dispatch = useDispatch();
+  const filtersStatus = useSelector((state) => state.filter);
+  const theme = useSelector((state) => state.themes.value);
+  const [filters, setFilters] = useState("");
+  const dateFilters = ["Сначала новые", "Сначала старые"];
+
+  const filterHandler = (e) => {
+    switch (props.id) {
+      case "author":
+        console.log(filtersStatus.nameFilter.includes(e.target.id));
+        console.log(filtersStatus.nameFilter);
+        console.log(e.target.id);
+        filtersStatus.nameFilter.includes(e.target.id)
+          ? dispatch(removeNameFilter(e.target.id))
+          : dispatch(setNameFilters(e.target.id));
+
+        break;
+      case "release_date":
+        filtersStatus.dateFilter === e.target.id
+          ? dispatch(removeDateFilter(e.target.id))
+          : dispatch(setDateFilters(e.target.id));
+        break;
+      case "genre":
+        filtersStatus.genreFilter === e.target.id
+          ? dispatch(removeGenreFilter(e.target.id))
+          : dispatch(setGenreFilters(e.target.id));
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://painassasin.online/catalog/track/all/`)
+      .then((response) => response.json())
+      .then((posts) => {
+        let cleanFilters = posts
+          .map((el) => el[props.id])
+          .filter(
+            (item, i, ar) => ar.indexOf(item) === i && item !== "-" && item
+          );
+        if (props.id === "release_date") {
+          cleanFilters = dateFilters;
+        }
+        setFilters(
+          cleanFilters.map((el) => (
+            <S.filterText id={el} onClick={filterHandler}>
+              {el}
+            </S.filterText>
+          ))
+        );
+      });
+  }, []);
+
+  return (
+    <S.filter>
+      <S.filterButton
+        theme={theme}
+        active={props.visible}
+        id={props.id}
+        onClick={props.clicker}
+      >
+        {props.text}
+      </S.filterButton>
+      {props.visible && (
+        <S.filterDropDown>
+          <S.filterContent>{filters}</S.filterContent>
+        </S.filterDropDown>
+      )}
+    </S.filter>
+  );
 }
 
-export default FilterButton
+export default FilterButton;
--- src/redux/reducers/filter_slice.js
+++ src/redux/reducers/filter_slice.js
@@ -3,23 +3,36 @@ import { createSlice } from "@reduxjs/toolkit";
 export const filterSlice = createSlice({
   name: "filters",
   initialState: {
-    nameFilter:[],
-    dataFilter:'',
-    genreFilter:'',
+    nameFilter: [],
+    dataFilter: "",
+    genreFilter: "",
   },
   reducers: {
-    setFilters: (state, action) => {
-     state.nameFilter.push(action.payload.names)
-     state.dataFilter = action.payload.data
-     state.genreFilter = action.payload.genre
+    setNameFilters: (state, action) => {
+      state.nameFilter.push(action.payload);
     },
 
- 
+    setDateFilters: (state, action) => {
+      state.dataFilter = action.payload;
+    },
 
+    setGenreFilters: (state, action) => {
+      state.genreFilter = action.payload;
+    },
+    removeNameFilter:(state,action) =>{
+      const i = state.nameFilter.indexOf(action.payload)
+      state.nameFilter.splice(i,1)
+    },
+    removeDateFilter:(state,action) =>{
+      state.dataFilter = ''
+    },
+    removeGenreFilter:(state,action) =>{
+      state.genreFilter = ''
+    },
 
   },
 });
 
-export const { setFilters} = filterSlice.actions;
+export const { setDateFilters,setGenreFilters,setNameFilters,removeDateFilter,removeGenreFilter,removeNameFilter} = filterSlice.actions;
 
 export default filterSlice.reducer;
